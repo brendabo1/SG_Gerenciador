fb5f4f7e5e5476d399f9c837616baa7f7b8c4366
Brenda Oliveira <1brendabarbosa@gmail.com>
2022-06-14 20:05:27 -0300

    primeiro commit

---
+++ src/application/controllers/ControllerTelaLogin.java
@@ -0,0 +1,15 @@
+package application.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ControllerTelaLogin {
+	@FXML
+    private TextField campoIDUser;
+
+    @FXML
+    private PasswordField campoSenha;
+
+    
+    
+}
